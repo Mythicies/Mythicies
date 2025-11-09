34183dd3f3aa373581ab1be9765459e8c65f419c
Mythicies <100194508+Mythicies@users.noreply.github.com>
2025-11-09 10:07:06 +0100

    start project RPG minigame

---
+++ Dagbesteding PC/C++/RPG Minigame/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Starting RPG!\n";
+    return 0;
+}
---
+++ Dagbesteding PC/C++/RPG Minigame/src/player.hpp
@@ -0,0 +1,15 @@
+#ifndef PLAYER_HPP
+#define PLAYER_HPP
+
+#include "Character.hpp"
+
+class Player : public Character
+{
+private:
+    /* data */
+public:
+    Player(/* args */);
+    ~Player();
+};
+
+#endif
