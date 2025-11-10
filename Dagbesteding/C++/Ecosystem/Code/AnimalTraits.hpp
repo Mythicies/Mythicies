1e081fb6ed53cd71f057daf32be52c18784eec1e
Mythicies <100194508+Mythicies@users.noreply.github.com>
2025-11-10 20:28:38 +0100

    ClassDiagram bad update

--- Dagbesteding/C++/Ecosystem/Code/AnimalTraits.hpp
+++ Dagbesteding/C++/Ecosystem/Code/AnimalTraits.hpp
@@ -3,14 +3,14 @@
 
 #include <unordered_map>
 
-const unordered_map<std::string, AnimalTraits> speciesDB;
-
 struct AnimalTraits
 {
     float hunger;
     float energy;
-    float lifesppan;
+    float lifespan;
     float age;
 };
+
+extern const std::unordered_map<std::string, AnimalTraits> speciesDB;
 
 #endif
--- Dagbesteding/C++/Ecosystem/Code/main.cpp
+++ Dagbesteding/C++/Ecosystem/Code/main.cpp
@@ -1,10 +1,11 @@
 #include "Animal.hpp"
 #include <iostream>
 #include "AnimalTraits.hpp"
+#include <unordered_map>
 
 int main()
 {
     Animal lion("Lion", speciesDB.at("Lion"));
     Animal deer("Deer", speciesDB.at("Deer"));
-    cout << lion.getType();
+    cout << lion.getType() << endl;
 }
